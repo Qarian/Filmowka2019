44a1929f8eabd08f6b07cbc97be5084620c5565c
Necr0o0 <radekklaser@gmail.com>
2019-09-07 00:08:53 +0200

    Basic view

---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    public float xRotationSpeed = 1;
+    public float yRotationSpeed = 1;
+    public float zRotationSpeed = 1;
+    void Update()
+    {
+        transform.Rotate(new Vector3(xRotationSpeed,yRotationSpeed,zRotationSpeed));
+    }
+}
