956a8803ce97fec42181aae1dc36f17ff7b3f6cb
Qarian <Ferno>
2019-09-07 22:27:47 +0200

    implemented most of audio

---
+++ Assets/Scripts/CageScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageScript : MonoBehaviour
+{
+	AudioSource source;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (source == null)
+			source = GetComponent<AudioSource>();
+		source.Play();
+
+		CharacterController controller = other.GetComponent<CharacterController>();
+		controller.enabled = false;
+		other.transform.position = new Vector3(0f, 1f, 0f);
+		controller.enabled = true;
+	}
+}
